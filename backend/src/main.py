7ff40a55fba9f24df04d9c050ba87d38f99cdcb8
Jason Schneider <jason@jasonswebsite.ca>
2022-05-08 12:11:45 -0300

    Removed some (nonfunctional) logging

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 import utils.scraper_utils as su
-import logging
 
-logger = logging.getLogger('api')
 app = FastAPI()
-
 
 @app.get('/fetch_posts')
 async def fetch_posts():
 	info = su.get_posts(1)
-	logger.debug(info)
 	return info
